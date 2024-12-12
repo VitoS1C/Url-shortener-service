0faacfc329e4c6b9498051f5a6c86cbe7eb59bd0
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-12-12 20:52:46 +0500

    Refactored

--- src/main/java/faang/school/urlshortenerservice/UrlShortenerServiceApplication.java
+++ src/main/java/faang/school/urlshortenerservice/UrlShortenerServiceApplication.java
@@ -5,15 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients("faang.school.urlshortenerservice.client")
 @EnableCaching
-@ComponentScan(basePackages = "faang.school.urlshortenerservice.client")
 public class UrlShortenerServiceApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(UrlShortenerServiceApplication.class)
